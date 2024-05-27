8d1dbbf2a2481921e3495c6ce16bb1c693af7688
Christian Tchilikov <christian.tchilikov@semify-eda.com>
2024-05-27 13:29:47 +0200

    update imu scripts

--- python/imu_readout.py
+++ python/imu_readout.py
@@ -23,21 +23,8 @@ try:
 except:
     com = None
 
-matplotlib.use('TkAgg')
-
 # If set to true, include the io-expander for visualization
 IO_EXPANDER = False
-
-# If set to true, the script generates and plays a sine wave according to the IMU sensor data
-PLAY_SOUND = False 
-
-# Color scheme for plotting
-sem_grey = '#323c40'
-sem_white = '#f2f2f2'
-sem_blue = '#43788c'
-sem_dark_blue = '#2b4c59'
-sem_light_grey = '#6d848c'
-
 
 def axl_conf(i2c, i2c_addr, odr: str = '12.5Hz', fs: str = '2g') -> None:
     """
@@ -88,39 +75,6 @@ def twos_comp(val, bits: int = 16):
     return val
 
 
-def generate_sound(frequency: int = 440, duration: float = 1, harmonics: int = 0, pitch_shift: int = 0):
-    """
-    Generate a sinusoidal signal from the user defined parameters
-    :param frequency: Fundamental frequency of the sine wave
-    :param duration: Duration of the signal in seconds
-    :param harmonics: Number of harmonics to add to the signal
-    :param pitch_shift: Pitch shift in octaves (positive for increase, negative for decrease)
-    :return: None
-    """
-    # Bluetooth speaker sample rate ~48kHz
-    fs = 20000    # Sampling frequency   44100
-    # Scale the period of the signal according to the pitch shift
-    if pitch_shift < 0:
-        duration = duration / (2 ** (pitch_shift * -1))
-
-    if pitch_shift > 0:
-        duration = duration * 2 ** pitch_shift
-
-    # Generate the fundamental sine wave
-    t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    signal_wave = np.sin(2 * np.pi * frequency * t)
-
-    for harmonic in range(2, harmonics + 1):
-        if harmonic % 2 == 1:
-            signal_wave += np.sin(2 * np.pi * frequency * harmonic * t) / harmonic
-
-    if pitch_shift != 0:
-        resample_factor = 2 ** -pitch_shift
-        signal_wave = signal.resample(signal_wave, int(len(signal_wave) * resample_factor))
-
-    return signal_wave
-
-
 def main():
     """
     The main function handles the connection to SmartWave and the ASM330LHHXG1 IMU Sensor. It configures the
@@ -158,71 +112,7 @@ def main():
         axl_conf(i2c_imu, i2c_imu_addr)
         gyro_conf(i2c_imu, i2c_imu_addr)
 
-        # Import semify logo for plotting
-        file = "../../IMU_sensor_demo/python/semify_logo.png"
-        img = Image.open(file)
-        resize = img.resize((np.array(img.size) / 19).astype(int))  # / 17
-
-        # Default settings for plotting
-        plt.rcParams["figure.facecolor"] = sem_grey
-        plt.rcParams["figure.figsize"] = [9.50, 7.50]
-        plt.rcParams["figure.autolayout"] = True
-        x_len = 200
-
-        fig = plt.figure()
-        gs = fig.add_gridspec(3, 2)
-        ax1 = fig.add_subplot(gs[0, 0])
-        ax2 = fig.add_subplot(gs[1, 0])
-        ax3 = fig.add_subplot(gs[2, 0])
-        ax4 = fig.add_subplot(gs[0, 1])
-        ax5 = fig.add_subplot(gs[1, 1])
-        ax6 = fig.add_subplot(gs[2, 1])
-
-        fig.suptitle("Inertial Measurement Unit\nData Visualization", color=sem_white)
-        ax1.set_title("X - axis", color=sem_white)
-        ax2.set_title("Y - axis", color=sem_white)
-        ax3.set_title("Z - axis", color=sem_white)
-
-        ax4.set_title("Pitch", color=sem_white)
-        ax5.set_title("Roll", color=sem_white)
-        ax6.set_title("Yaw", color=sem_white)
-
-        for ax in [ax1, ax2, ax3]:
-            ax.set_ylim(-20, 20)
-            ax.set_ylabel("Acceleration (m/s^2)", color=sem_white)
-            ax.set_xlabel("Samples", color=sem_white)
-            ax.tick_params(labelcolor=sem_white)
-            ax.grid()
-
-        for ax in [ax4, ax5, ax6]:
-            ax.set_ylim(-300, 300)
-            ax.set_ylabel("Rate of Change", color=sem_white)
-            ax.yaxis.set_label_position('right')
-            ax.yaxis.tick_right()
-            ax.set_xlabel("Samples", color=sem_white)
-            ax.tick_params(labelcolor=sem_white)
-            ax.grid()
-
-        xs = list(range(0, 200))
-
-        axl_x = [0] * x_len
-        axl_y = [0] * x_len
-        axl_z = [0] * x_len
-        line1, = ax1.plot(xs, axl_x, lw=2, color=sem_dark_blue)
-        line2, = ax2.plot(xs, axl_y, lw=2, color=sem_blue)
-        line3, = ax3.plot(xs, axl_z, lw=2, color=sem_light_grey)
-
-        gyro_x = [0] * x_len
-        gyro_y = [0] * x_len
-        gyro_z = [0] * x_len
-        line4, = ax4.plot(xs, gyro_x, lw=2, color=sem_dark_blue)
-        line5, = ax5.plot(xs, gyro_y, lw=2, color=sem_blue)
-        line6, = ax6.plot(xs, gyro_z, lw=2, color=sem_light_grey)
-
-        line = [line1, line2, line3, line4, line5, line6]
-        ys = [axl_x, axl_y, axl_z, gyro_x, gyro_y, gyro_z]
-
-        def animate(i, ys):
+        while(1):
             """
             This function is used to animate the plotting, which enables displaying real-time data.
             :param i: Placeholder
@@ -232,18 +122,24 @@ def main():
 
             try:
 
-                pitch_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x22.to_bytes(1, 'big'), 1)
-                pitch_msb = i2c_imu.readRegister(i2c_imu_addr, 0x23.to_bytes(1, 'big'), 1)
+                # pitch_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x22.to_bytes(1, 'big'), 1)
+                # pitch_msb = i2c_imu.readRegister(i2c_imu_addr, 0x23.to_bytes(1, 'big'), 1)
+                pitch_lsb = bytes([0]) 
+                pitch_msb =  bytes([0]) 
                 pitch = (pitch_msb[0] << 8) + pitch_lsb[0]
                 tc_pitch = twos_comp(pitch)
 
-                roll_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x24.to_bytes(1, 'big'), 1)
-                roll_msb = i2c_imu.readRegister(i2c_imu_addr, 0x25.to_bytes(1, 'big'), 1)
+                # roll_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x24.to_bytes(1, 'big'), 1)
+                # roll_msb = i2c_imu.readRegister(i2c_imu_addr, 0x25.to_bytes(1, 'big'), 1)
+                roll_lsb = bytes([0]) 
+                roll_msb = bytes([0]) 
                 roll = (roll_msb[0] << 8) + roll_lsb[0]
                 tc_roll = twos_comp(roll)
 
-                yaw_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x26.to_bytes(1, 'big'), 1)
-                yaw_msb = i2c_imu.readRegister(i2c_imu_addr, 0x27.to_bytes(1, 'big'), 1)
+                # yaw_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x26.to_bytes(1, 'big'), 1)
+                # yaw_msb = i2c_imu.readRegister(i2c_imu_addr, 0x27.to_bytes(1, 'big'), 1)
+                yaw_lsb = bytes([0]) 
+                yaw_msb = bytes([0]) 
                 yaw = (yaw_msb[0] << 8) + yaw_lsb[0]
                 tc_yaw = twos_comp(yaw)
 
@@ -254,31 +150,23 @@ def main():
                 # print(f"Pitch: {tc_pitch:.3f}     Roll: {tc_roll:.3f}    Yaw: {tc_yaw:.3f}")
                 # print(f"Pitch: {pitch_res:.3f} degrees    Roll: {roll_res:.3f} degrees   Yaw: {yaw_res:.3f} degrees\n")
 
-                ys[3].append(pitch_res)
-                ys[3] = ys[3][-x_len:]
-                line[3].set_ydata(ys[3])
-
-                ys[4].append(roll_res)
-                ys[4] = ys[4][-x_len:]
-                line[4].set_ydata(ys[4])
-
-                ys[5].append(yaw_res)
-                ys[5] = ys[5][-x_len:]
-                line[5].set_ydata(ys[5])
-
                 # Linear acceleration sensor
                 x_axis_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x28.to_bytes(1, 'big'), 1)
                 x_axis_msb = i2c_imu.readRegister(i2c_imu_addr, 0x29.to_bytes(1, 'big'), 1)
                 x_axis = (x_axis_msb[0] << 8) + x_axis_lsb[0]
                 tc_x_axis = twos_comp(x_axis, 16)
 
-                y_axis_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x2A.to_bytes(1, 'big'), 1)
-                y_axis_msb = i2c_imu.readRegister(i2c_imu_addr, 0x2B.to_bytes(1, 'big'), 1)
+                # y_axis_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x2A.to_bytes(1, 'big'), 1)
+                # y_axis_msb = i2c_imu.readRegister(i2c_imu_addr, 0x2B.to_bytes(1, 'big'), 1)
+                y_axis_lsb = bytes([0]) 
+                y_axis_msb = bytes([0]) 
                 y_axis = (y_axis_msb[0] << 8) + y_axis_lsb[0]
                 tc_y_axis = twos_comp(y_axis, 16)
 
-                z_axis_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x2C.to_bytes(1, 'big'), 1)
-                z_axis_msb = i2c_imu.readRegister(i2c_imu_addr, 0x2D.to_bytes(1, 'big'), 1)
+                # z_axis_lsb = i2c_imu.readRegister(i2c_imu_addr, 0x2C.to_bytes(1, 'big'), 1)
+                # z_axis_msb = i2c_imu.readRegister(i2c_imu_addr, 0x2D.to_bytes(1, 'big'), 1)
+                z_axis_lsb = bytes([0])  
+                z_axis_msb = bytes([0]) 
                 z_axis = (z_axis_msb[0] << 8) + z_axis_lsb[0]
                 tc_z_axis = twos_comp(z_axis, 16)
 
@@ -287,44 +175,12 @@ def main():
                 x_res = tc_x_axis * axl_sens['2g'] * 9.80665
                 y_res = tc_y_axis * axl_sens['2g'] * 9.80665
                 z_res = tc_z_axis * axl_sens['2g'] * 9.80665
-                # print(f"X_adc: {pitch:.3f}     Y_adc: {roll:.3f}    Z_adc:: {yaw:.3f}")
                 # print(f"X_a: {x_res:.3f} m/s^2    Y_a: {y_res:.3f} m/s^2   Z_a: {z_res:.3f} m/s^2\n")
                 print(f"x axis: {hex(x_axis)}    y axis: {hex(y_axis)}    z axis: {hex(z_axis)}")
-                # print("SoC Core Enable (0x48010): \n0x%x\n" % (int(sw.readFPGARegister(0x48010)) >> 1))
-                ys[0].append(y_res)
-                ys[0] = ys[0][-x_len:]
-                line[0].set_ydata(ys[0])
-
-                ys[1].append(x_res)
-                ys[1] = ys[1][-x_len:]
-                line[1].set_ydata(ys[1])
-
-                ys[2].append(z_res)
-                ys[2] = ys[2][-x_len:]
-                line[2].set_ydata(ys[2])
 
                 if IO_EXPANDER:
                     io_led_toggle(i2c_io_exp, i2c_io_exp_addr, y_res, x_res)
 
-                if PLAY_SOUND:
-                    frequency = 220
-                    duration = 0.1
-
-                    harmonics, pitch_shift = sound_modulation(y_res, x_res)
-
-                    signal_wave = generate_sound(frequency, duration, harmonics, pitch_shift)
-
-                    p = pyaudio.PyAudio()
-                    stream = p.open(format=pyaudio.paFloat32,
-                                    channels=1,
-                                    rate=20000,
-                                    output=True)
-                    stream.write(signal_wave.astype(np.float32).tobytes())
-                    stream.stop_stream()
-
-                return line
-
-               
             except Exception as e:
                 print("Connection to IMU unit has been interrupted. Terminating Program.")
                 print(e)
@@ -337,18 +193,6 @@ def main():
                     print("Unable to read contents of I2CT debug register...")
                 SmartWave.disconnect(sw)
                 exit()
-
-        anim = animation.FuncAnimation(fig, animate,
-                                    fargs=(ys,),
-                                    interval=1,
-                                    blit=True,
-                                    cache_frame_data=False)
-
-        # plt.figimage(resize, xo=int(fig.bbox.xmax // 2) + 250, yo=int(fig.bbox.ymax) + 220)
-        # plt.figimage(resize, xo=int(fig.bbox.xmax // 2) + 650, yo=int(fig.bbox.ymax) + 220)
-        plt.figimage(resize, origin='upper')
-        # plt.get_current_fig_manager().full_screen_toggle()
-        plt.show()
 
 if __name__ == "__main__":
     process = Process(target=main)
